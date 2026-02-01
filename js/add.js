f331b876016dbd6e7d1b1ccd9c84c8d6b5334800
elzodbekahmedov-hash <elzodbekahmedov@gmail.com>
2026-02-01 09:54:14 +0500

    upgrade commit

--- js/add.js
+++ js/add.js
@@ -1,6 +1,6 @@
     const elForm = document.getElementById("form");
 
-      elForm.addEventListener("submit", (evt) => {
+      elForm.addEventListener("Submit", (evt) => {
         evt.preventDefault();
         const formData = new FormData(elForm);
         const result = {};
@@ -20,3 +20,47 @@
           location.href = "/index.html";
         });
       });
+
+
+
+
+  
+  // const elForm = document.getElementById("form");
+
+  // elForm.addEventListener("Submit", async (evt) => {
+  //   evt.preventDefault();
+
+  //   const formData = new FormData(elForm);
+  //   const result = Object.fromEntries(formData.entries());
+
+  //   result.isWild = elForm.elements.isWild.checked;
+
+  //   result.speed = Number(result.speed);
+  //   result.year = Number(result.year);
+  //   result.weight = Number(result.weight);
+
+  //   const token = localStorage.getItem("token");
+  //   if (!token) {
+  //     alert("Token topilmadi. Login qiling.");
+  //     location.href = "/login.html";
+  //     return;
+  //   }
+
+  //   const res = await fetch("https://json-api.uz/api/project/game-over/animals", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Bearer " + token,
+  //     },
+  //     body: JSON.stringify(result),
+  //   });
+
+  //   if (!res.ok) {
+  //     const err = await res.json().catch(() => ({}));
+  //     alert(err?.message || "Xatolik: ma'lumot yuborilmadi");
+  //     return;
+  //   }
+
+  //   location.href = "/index.html";
+  // });
+
